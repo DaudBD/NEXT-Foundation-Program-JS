b851885e85f2d6ef3f52bb0a1f223bd7728244fb
Md Ariful Islam <arifuldaud@gmail.com>
2026-08-23 07:31:26 +0600

    first commit

---
+++ Variable.js
@@ -0,0 +1,14 @@
+let firstName ='Arif'
+name2 ='Daud';
+//console.log(firstName);
+const age = 21;
+// age = 20;
+//console.log(age);
+
+const isAdult =true;
+console.log(typeof(name2),typeof(age),typeof(isAdult));
+
+let schoolName ='Adachaki';
+
+console.log(schoolName)
+
