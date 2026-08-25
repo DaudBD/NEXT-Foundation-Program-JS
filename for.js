f29cfaed9b806662df33241817dba10222609fc3
Md Ariful Islam <arifuldaud@gmail.com>
2026-08-25 22:32:37 +0600

    update

---
+++ for.js
@@ -0,0 +1,38 @@
+
+/*let tech =["HTML","CSS","JS","TS"];
+for ( let x of tech){
+    console.log("I am Learning",x);
+}
+
+let tech = ["HTML", "CSS", "JS", "TS"];
+
+tech.forEach(function(x) {
+    console.log("I am Learning", x);
+});
+
+let obj = {
+ name : "Arif",
+ age : 30,
+ Address: "Dhaka",
+
+}
+for (let x in obj) {
+    console.log(x,"->",obj[x]);
+}
+//Break 
+ let marks = 45;
+
+ for (let i=marks; i <= 50; i++){
+if ( i < 100){
+    break;
+}
+
+    console.log(marks);
+ }*/
+
+ for ( let i= 1; i < 15; i++) {
+    if (i%2 !=0) {
+        continue;
+    }
+    console.log(i);
+ }
---
+++ loop.js
@@ -0,0 +1,25 @@
+
+//for (let i = 0; i < 5; i++) 
+
+//for (let i = 10; i >= 1; i--)
+//{
+   // console.log(`Count:,${i}`);
+//}
+
+let fruites = ["Mango","Apple","Orange","Tomamto"];
+
+//for (let i = 0; i < fruites.length;i++){
+//console.log("I Eat ", fruites[i]);
+//}
+
+//let i =0;
+//while (i < fruites.length) {
+//console.log("I Eat ", fruites[i]);
+//i++;
+//}
+
+let a = 5;
+while (a > 10) {
+    console.log("Count",a);
+a++;
+}
