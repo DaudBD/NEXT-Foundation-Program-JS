62388d6881266436ead2d88c9475084d92fdf4a6
Md Ariful Islam <arifuldaud@gmail.com>
2026-08-28 07:02:39 +0600

    update

---
+++ export.js
@@ -0,0 +1,17 @@
+//import add from "./math.js";
+
+import add from "./math.js";
+
+console.log(add(6,9));
+
+import {subs} from "./math.js";
+
+console.log(subs(5,9));
+
+import {pi} from "./math.js";
+
+console.log((pi));
+import add, { subs, pi } from "./math.js";
+console.log(add(10, 20));
+console.log(subs(20, 10));
+console.log(pi);
